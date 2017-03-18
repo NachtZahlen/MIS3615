d8eba09a22e950675d96a1326cdbc0e90e6f6911
Zhi Li <lizhiblack@gmail.com>
2017-03-18 00:52:06 -0400

    add day1

---
+++ day_1/guess_number.py
@@ -0,0 +1,21 @@
+# Guess My Number
+#
+# The computer picks a random number between 1 and 1000
+# The player tries to guess it and the computer lets
+# the player know if the guess is too high, too low
+# or right on the money.
+
+# Example:
+#
+# Hello! What is your name?
+# >>>James
+# Well, James, I am thinking of a number between 1 and 100.
+# Take a guess.
+# >>>50
+# Your guess is too high.
+# Take a guess.
+# >>>20
+# Your guess is too low.
+# Take a guess.
+# >>>42
+# Good job, James! You guessed my number in 3 guesses!
